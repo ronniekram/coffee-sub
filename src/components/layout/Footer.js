9536e17f42f021b0d0b0288d9282ca3d54c6d4d7
Arynn Boniface <arynn.boniface@gmail.com>
2021-09-18 02:00:18 -0400

    adding css files

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { ReactComponent as Twitter } from '../../assets/shared/desktop/icon-twitter.svg';
+import { ReactComponent as Instagram } from '../../assets/shared/desktop/icon-instagram.svg';
+import { ReactComponent as Facebook } from '../../assets/shared/desktop/icon-facebook.svg';
+import './css/footer.css';
 
 const Footer = () => {
   return (
