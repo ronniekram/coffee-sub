75e5fbd9638963578b1829b47de58103423ce350
Arynn Boniface <arynn.boniface@gmail.com>
2021-09-18 01:22:21 -0400

    creating outline for nav bar

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ReactComponent as Hamburger } from '../../assets/shared/mobile/icon-hamburger.svg';
+import { ReactComponent as CloseMenu } from '../../assets/shared/mobile/icon-close.svg';
+import { ReactComponent as Logo } from '../../assets/shared/desktop/logo.svg';
+import '../../assets/styles/layout/nav.css';
 
 const NavBar = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <div>
-      
-    </div>
+    <nav className="container">
+
+      <div className="logo">
+        <Logo />
+      </div>
+
+      <div className="links">
+        <ul>
+          <li>HOME</li>
+          <li>ABOUT US</li>
+          <li>CREATE YOUR PLAN</li>
+        </ul>
+      </div>
+
+      <div className="mobile-links">
+        <ul>
+          <li>HOME</li>
+          <li>ABOUT US</li>
+          <li>CREATE YOUR PLAN</li>
+        </ul>
+      </div>
+
+    </nav>
   );
 };
 
