50eda393728b2669a2fbc0b40a040abdb41f4827
Arynn Boniface <arynn.boniface@gmail.com>
2021-09-18 00:20:18 -0400

    adding more skeletons

---
+++ src/components/about/Headquarters.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Headquarters = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default Headquarters;
---
+++ src/components/plan/Step.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Step = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default Step;
